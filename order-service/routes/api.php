07a03dc5d58c8d5c465012883b280fff32edb0eb
Diego Emerick <diegoemericksousa@hotmail.com>
2017-11-12 19:51:29 -0200

    created routes offer

--- order-service/routes/api.php
+++ order-service/routes/api.php
@@ -5,3 +5,7 @@ Route::post('/v1/order', 'ServiceOrderController@create');
 Route::put('/v1/order/{id}', 'ServiceOrderController@update');
 Route::get('/v1/order/{id}', 'ServiceOrderController@get');
 Route::delete('/v1/order/{id}', 'ServiceOrderController@delete');
+
+Route::get('/v1/offer', 'OrderLawyerOfferController@listOffers');
+Route::post('/v1/offer', 'OrderLawyerOfferController@createOrderOffer');
+Route::get('/v1/offer/{orderId}', 'OrderLawyerOfferController@listOffersByOrder');
